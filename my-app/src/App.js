4d341732d2ee1a443f63d44db17793e7428f627b
Alexey <alexeyBarsAlexey@mail.ru>
2022-06-09 20:19:48 +0300

    started the project

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FormOfClient from './components/FormOfClient';
+
+function App() {
+  return (
+    <div className="App">
+      <FormOfClient />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/FormOfClient.js
@@ -0,0 +1,13 @@
+
+function FormOfClient() {
+  return (
+    <>
+      <form>
+        <h4>Заполните форму:</h4>
+        <input type='text' placeholder='Введите ваше ФИО' />
+      </form>
+    </>
+  )
+};
+
+export default FormOfClient;
